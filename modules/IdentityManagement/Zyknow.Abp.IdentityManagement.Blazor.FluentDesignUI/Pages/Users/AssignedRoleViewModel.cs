ae1a2782d33334dc5008ec330a295d3308a817e9
zyknow <1047606598@qq.com>
2025-03-25 18:11:30 +0800

    add user management dialogs and role view model

---
+++ modules/IdentityManagement/Zyknow.Abp.IdentityManagement.Blazor.FluentDesignUI/Pages/Users/AssignedRoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Zyknow.Abp.IdentityManagement.Blazor.FluentDesignUI.Pages.Users;
+
+public class AssignedRoleViewModel
+{
+    public string Name { get; set; }
+
+    public bool IsAssigned { get; set; }
+}
