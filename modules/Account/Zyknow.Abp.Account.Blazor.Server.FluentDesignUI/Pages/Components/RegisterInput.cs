eb7ab62902d33da6c44629d0ba723b83c1fe1271
zyknow <1047606598@qq.com>
2025-03-31 18:24:04 +0800

    Add registration input component and update login logic for tenant support

---
+++ modules/Account/Zyknow.Abp.Account.Blazor.Server.FluentDesignUI/Pages/Components/RegisterInput.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Volo.Abp.Auditing;
+using Volo.Abp.Identity;
+using Volo.Abp.Validation;
+
+namespace Zyknow.Abp.Account.Blazor.Server.FluentDesignUI.Pages.Components;
+
+public class RegisterInput
+{
+    [Required]
+    [DynamicStringLength(typeof(IdentityUserConsts), nameof(IdentityUserConsts.MaxUserNameLength))]
+    public string UserName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [DynamicStringLength(typeof(IdentityUserConsts), nameof(IdentityUserConsts.MaxEmailLength))]
+    public string EmailAddress { get; set; }
+
+    [Required]
+    [DynamicStringLength(typeof(IdentityUserConsts), nameof(IdentityUserConsts.MaxPasswordLength))]
+    [DataType(DataType.Password)]
+    [DisableAuditing]
+    public string Password { get; set; }
+}
